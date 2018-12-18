3522ce6d97f3c42653da315a0860a1a655268e83
yuyanan1314 <18511893521@sina.cn>
2018-12-19 02:24:00 +0800

    Signed-off-by: yuyanan1314 <18511893521@sina.cn>

---
+++ stream/springcloud-2.0-consul-order/src/main/java/com/yyn/order/Application.java
@@ -0,0 +1,39 @@
+package com.yyn.order;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@SpringBootApplication
+public class Application {
+
+	@Value("${server.port}")
+	private String port;
+
+	@RequestMapping("/getOrder")
+	public String getMember() {
+		return restTemplate.getForEntity("http://consul-member/getMember", String.class).getBody();	}
+	
+	@Autowired
+    private RestTemplate restTemplate;
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+}
